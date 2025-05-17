c46ec3ca56533ea60f4b619e182475b362a18f1d
Gab-Bit-bit <gabideconto04@gmail.com>
2025-05-16 21:33:01 -0300

    adicionado historico dos dentistas

--- src/components/QuemCuida.tsx
+++ src/components/QuemCuida.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./QuemCuida.css";
 import { Link } from "react-router-dom";
@@ -8,23 +7,12 @@ import dentista3 from "../img/dentista3.png";
 import dentista4 from "../img/dentista4.png";
 import dentista5 from "../img/dentista5.png";
 import dentista6 from "../img/dentista6.png";
-=======
-import React from 'react';
-import './QuemCuida.css';
-import dentista1 from '../img/dentista1.png';
-import dentista2 from '../img/dentista2.png';
-import dentista3 from '../img/dentista3.png';
-import dentista4 from '../img/dentista4.png';
-import dentista5 from '../img/dentista5.png';
-import dentista6 from '../img/dentista6.png';
->>>>>>> 5090b1c95ad606d0270edc620dc3aaf288617822
 
 const QuemCuida: React.FC = () => {
   return (
     <section className="quem-cuida">
       <h2>Profissionais</h2>
       <div className="dentistas-container">
-<<<<<<< HEAD
         <div className="perfilProfissional">
           <Link to="/perfilProfissional/dr-joao-silva" className="perfilProfssional">
             <img src={dentista1} alt="JoaoSilva" />
@@ -61,45 +49,10 @@ const QuemCuida: React.FC = () => {
           <img src={dentista6} alt="Dra. Luana de Conto" />
           <h3>Dra. Luana de Conto</h3>
           <p>Clinico Geral</p></Link>
-=======
-        <div className="dentista-card">
-          <img src={dentista1} alt="Dr.João Silva" />
-          <h3>Dr. João Silva</h3>
-          <p>Especialista em Ortodontia</p>
-        </div>
-        <div className="dentista-card">
-          <img src={dentista2} alt="Dra. Mario Santos" />
-          <h3>Dra. Mario Santos</h3>
-          <p>Especialista em Implantes</p>
-        </div>
-        <div className="dentista-card">
-          <img src={dentista3} alt="Dra. Ana Oliveira" />
-          <h3>Dra. Ana Oliveira</h3>
-          <p>Especialista em Periodontia</p>
-        </div>
-        <div className="dentista-card">
-          <img src={dentista4} alt="Dr. Carlos Mendes" />
-          <h3>Dr. Carlos Mendes</h3>
-          <p>Especialista em Cirurgia</p>
-        </div>
-        <div className="dentista-card">
-          <img src={dentista5} alt="Dra. Paula Costa" />
-          <h3>Dra. Paula Costa</h3>
-          <p>Especialista em Endodontia</p>
-        </div>
-        <div className="dentista-card">
-            <img src={dentista6} alt="Dra. Luana de Conto" />
-            <h3>Dra. Luana de Conto</h3>
-            <p>Especialista em Protese e Implantes</p>
->>>>>>> 5090b1c95ad606d0270edc620dc3aaf288617822
         </div>
       </div>
     </section>
   );
 };
 
-<<<<<<< HEAD
 export default QuemCuida;
-=======
-export default QuemCuida; 
->>>>>>> 5090b1c95ad606d0270edc620dc3aaf288617822
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,26 +3,17 @@ import './Home.css';
 import mainbanner from "../img/mainbanner.jpg";
 import Navbar from '../components/Nav'; // Certifique-se de que esse caminho está correto
 import LoginModal from '../components/LoginModal';
-<<<<<<< HEAD
 import QuemCuida from '../components/QuemCuida';
 import AreasDeAtuacao from '../components/AreasDeAtuacao';
 
 const HomeContent: React.FC = () => {
-=======
-
-const Home: React.FC = () => {
->>>>>>> 5090b1c95ad606d0270edc620dc3aaf288617822
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
   return (
     <div className="home-container">
-<<<<<<< HEAD
       {/*tirei o Navbar (o bgl de login ta no nav.tsx*/}
-=======
-      {/* Navbar */}
-      <Navbar onLoginClick={() => setIsLoginModalOpen(true)} />
+      
 
->>>>>>> 5090b1c95ad606d0270edc620dc3aaf288617822
       {/* Hero Section */}
       <section className="hero">
         <div className="overlay"></div>
@@ -42,19 +33,11 @@ const Home: React.FC = () => {
       </section>
 
       {/* Login Modal */}
-<<<<<<< HEAD
       
-=======
-      <LoginModal
-        isOpen={isLoginModalOpen}
-        onClose={() => setIsLoginModalOpen(false)}
-      />
->>>>>>> 5090b1c95ad606d0270edc620dc3aaf288617822
     </div>
   );
 };
 
-<<<<<<< HEAD
 //o que tava feito no app.tsx
 export default function Home() {
 return (
@@ -65,8 +48,3 @@ return (
   </>
 );
 }
-
-
-=======
-export default Home;
->>>>>>> 5090b1c95ad606d0270edc620dc3aaf288617822
